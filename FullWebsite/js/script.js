b100b2ca99366457f35f4ce8b9c3616d8b4f2a7c
Stephan Yu <stephan.yu@gmail.com>
2014-07-09 18:12:04 +0200

    Update Font and sub-shopping item
    added Charcoal font to stylesheet and added some additional
    functionality to the sub-shopping item

--- FullWebsite/js/script.js
+++ FullWebsite/js/script.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
   
-  
   //if click on submit button 1) add value of input text entries to shopping-item div
   //then 2) make div shopping item appear inside of shopping-list div
   var counter = 0;
@@ -11,7 +10,7 @@ $(document).ready(function() {
       var quant = $("#quantity").val();
       var measr = $("#measurement").val();
       var string_tot = value + " " + quant + measr;
-      $("#shopping-list").append("<div class='shopping-item-container'><div class='shopping-item'><p class='receptor'>" + string_tot + "</p><div class='checkbox'><input type='checkbox' value='1' id='" + checkbox + "'/><label for='" + checkbox + "' class='label'></label></div><i class='fa fa-trash-o'></i><i class='fa fa-bars'></i></div><div class='sub-shopping-item hidden'><form id='sub-shopping-form" + counter + "'><input type='text' name='Note' class='note' placeholder='Note'><input type='date' class='date'></input><i class='fa fa-plus'></i><i class='fa fa-repeat'></i></form><p class='sub-shopping-notefield'></p></div></div>");
+      $("#shopping-list").append("<div class='shopping-item-container'><div class='shopping-item'><p class='receptor'>" + string_tot + "</p><div class='checkbox'><input type='checkbox' value='1' id='" + checkbox + "'/><label for='" + checkbox + "' class='label'></label></div><i class='fa fa-trash-o'></i><i class='fa fa-bars'></i></div><div class='sub-shopping-item hidden'><form id='sub-shopping-form" + counter + "'><input type='text' name='Note' class='note' placeholder='Note'><input type='date' class='date'></input><i class='fa fa-plus'></i><i class='fa fa-repeat'></i></form></div></div>");
       event.preventDefault();
       $("#add-item").val('');
       $("#quantity").val('');
@@ -19,38 +18,31 @@ $(document).ready(function() {
   });
 
   //if click on checkbox make 1)shopping item grey out and 2)strike-out text item  and 3)move shopping item to bottom of shopping list div
-  // $(document).on('click', function(event){   
-  //       $(event.target).closest('.receptor').toggleClass('strikethrough');
-  // });//Why does the above function not work when I click on the checkbox? It only works when I click on the Item description (.receptor)
-  $('#shopping-list').on('click', '.checkbox', function(){
-      $(this).closest('.shopping-item p').toggleClass('strikethrough');
-  }); //Why does this function not work?
-
+  $('#shopping-list').on('click', ':checkbox', function( event ){
+    $(this).closest('.shopping-item').children('p').toggleClass('strikethrough', this.checked );
+  });
   
   //if click on note logo then make submenu appear/disappear
-  // $(".fa-bars").click(function() {
-  //   $(this).closest("sub-shopping-item").toggleClass("hidden");
-  // }); Why does this not work?
+  $("#shopping-list").on('click', '.fa-bars', function(){
+    $(this).closest(".shopping-item").next().toggleClass("hidden"); 
+  });
 
-$("#shopping-list").on('click', '.fa-bars', function(){
-  $(this).closest(".shopping-item").next().toggleClass("hidden"); //why does it work when i choose #shopping-list but not when I choose .shopping-item-container??
-});
-
-
-  //if click submit2 button then save value of note and date to sub-shopping item which will expand and contract according to content
+  //if click submit2 button then save value of note and date to sub-shopping item
   $('#shopping-list').on('click','.fa-plus', function(){
-    // var value = $(this).closest("sub-shopping-form", "note").val();
-    // alert(value);
-    $(this).closest(".sub-shopping-item").toggleClass("expand");
-
+    var note = $(this).closest(".sub-shopping-item").find(".note").val();
+    var date = $(this).closest(".sub-shopping-item").find(".date").val();
+    var value = note + " " + date;
+    $(this).closest(".sub-shopping-item").toggleClass("expand")
+                                         .append("<div class='sub-shopping-notefield'></div>")
+                                         .children(".sub-shopping-notefield").text(value);
+    
   });
 
   //if click on bin button then remove the current shopping item
   $('#shopping-list').on('click','.fa-trash-o', function(){
-      $(this).closest('.shopping-item-container').slideUp('normal');
+    $(this).closest('.shopping-item-container').slideUp('normal');
   });
 
-  
 
 });
   
